cc8d9705ef96873d9eec238546feaf0bfac80e27
Gilbert King <gilbertking91@gmail.com>
2020-04-30 17:08:50 -0500

    version clean

--- index.py
+++ index.py
@@ -1,12 +1,34 @@
 import random 
 
-random_number = random.randint(1,10)
+random_odd_number = random.randint(1,10)
 guess = None
 
 while True: 
     guess = input("pick a number from 1 to 10: ")
     guess = int(guess)
-    if guess < random_number:
+    if guess < random_odd_number:
+        print("too low!")
+    elif: guess > random_odd_number:
+        print("too high!")
+else: 
+        print("You won!")
+        play_again = input("Do you wanna play again? (y/n) ")
+        if play_again == "y":
+        random_odd_number = random.randint(1,10)
+        guess = None
+else:
+        print("Thanks for playing!")
+break 
+
+print(random_odd_number)
+
+random_even_number = random.randint(1,10)
+guess = None
+
+while True: 
+    guess = input("pick a number from 1 to 10: ")
+    guess = int(guess)
+    if guess < random_even_number:
         print("too low!")
     elif: guess > random_number:
         print("too high!")
@@ -14,10 +36,80 @@ else:
         print("You won!")
         play_again = input("Do you wanna play again? (y/n) ")
         if play_again == "y":
-        random_number = random.randint(1,10)
+        random_even_number = random.randint(1,10)
         guess = None
 else:
         print("Thanks for playing!")
 break 
 
-print(random_number)
+print(random_even_number)
+
+
+
+random_cron = random.randint(1,10)
+guess = None
+
+while True: 
+    guess = input("pick a number from 1 to 10: ")
+    guess = int(guess)
+    if guess < random_cron:
+        print("too low!")
+    elif: guess > random_cron:
+        print("too leveraged !")
+else: 
+        print("You won!")
+        play_again = input("Do you wanna play again? (y/n) ")
+        if play_again == "y":
+        random_cron = random.randint(1,10)
+        guess = None
+else:
+        print("Thanks for playing!")
+break 
+
+print(random_cron)
+
+
+random_forest = random.randint(1,10)
+guess = None
+
+while True: 
+    guess = input("pick a number from 1 to 10: ")
+    guess = int(guess)
+    if guess < random_forest:
+        print("too low!")
+    elif: guess > random_forest:
+        print("too high!")
+else: 
+        print("You won!")
+        play_again = input("Do you wanna play again? (y/n) ")
+        if play_again == "y":
+        random_forest = random.randint(1,10)
+        guess = None
+else:
+        print("Thanks for playing!")
+break 
+
+print(random_forest)
+
+
+random_fox = random.randint(1,10)
+guess = None
+
+while True: 
+    guess = input("pick a number from 1 to 10: ")
+    guess = int(guess)
+    if guess < random_fox:
+        print("too low!")
+    elif: guess > random_number:
+        print("too high!")
+else: 
+        print("You won!")
+        play_again = input("Do you wanna play again? (y/n) ")
+        if play_again == "y":
+        random_fox = random.randint(1,10)
+        guess = None
+else:
+        print("Thanks for playing!")
+break 
+
+print(random_fox)
